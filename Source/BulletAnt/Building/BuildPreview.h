bcf44a62db29ea0dfa027d4af6e5d273c2c07bca
Derden-95 <khkc9597@gmail.com>
2026-01-26 18:49:49 +0900

    Add BuildManager & Building Preview
    - Build 모드 진입 후 건물 프리뷰 보여주기
    - Build 모드에서 선택된 건물 설치하기

---
+++ Source/BulletAnt/Building/BuildPreview.h
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BuildPreview.generated.h"
+
+class UBuildingData;
+class UStaticMeshComponent;
+class UMaterialInterface;
+class UMaterialInstanceDynamic;
+
+UCLASS()
+class BULLETANT_API ABuildPreview : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	ABuildPreview();
+
+	void InitWithData(UBuildingData* InData);
+	void UpdateTransform(const FVector& Location, const FRotator& Rotation);
+	void SetCanPlace(bool bInCanPlace);
+
+	bool CanPlace() const { return bCanPlace; }
+	float GetPlacementRadius() const;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY(EditDefaultsOnly)
+	UMaterialInterface* PreviewBaseMaterial;
+
+	UPROPERTY()
+	UMaterialInstanceDynamic* MID;
+
+	UPROPERTY()
+	UBuildingData* Data;
+
+	bool bCanPlace = false;
+};
