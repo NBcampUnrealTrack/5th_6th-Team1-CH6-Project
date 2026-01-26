09329422d5b05b0423a05d973383bdc33bece3c5
Soggeum <joyten0@gmail.com>
2026-01-26 15:03:05 +0900

    Add
    - GAS, StateTree, MassEntity 모듈 사용
    - 테스트 맵 생성
    - BaseEnemy BP 생성

--- Source/BulletAnt/BulletAnt.Build.cs
+++ Source/BulletAnt/BulletAnt.Build.cs
@@ -8,7 +8,27 @@ public class BulletAnt : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AnimGraphRuntime" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore", 
+			"EnhancedInput", 
+			"AnimGraphRuntime",
+			
+			// GAS
+			"GameplayAbilities", 
+			"GameplayTags", 
+			"GameplayTasks",
+			
+			// AI
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"NavigationSystem",
+			"MassEntity", "MassCommon", "MassNavigation", "MassMovement", "MassSpawner", "MassActors",
+			"MassAIBehavior", "MassRepresentation", "MassLOD", "MassSignals"
+		});
 
 		PublicIncludePaths.AddRange(new string[] {
             "BulletAnt" });
