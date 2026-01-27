7592940a4751a9b794b68f9a42b230c45ed2f1a3
Soggeum <joyten0@gmail.com>
2026-01-27 15:50:05 +0900

    Add SpawnerData
    - 스포너가 웨이브당 적 스폰할 때 필요한 정보를 담은 DataTable

---
+++ Source/BulletAnt/Enemy/Spawn/EnemySpawnerEntry.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Engine/DataTable.h"
+#include "Enemy/BaseEnemyCharacter.h"
+#include "EnemySpawnerEntry.generated.h"
+
+class ABaseEnemyCharacter;
+
+USTRUCT(BlueprintType)
+struct BULLETANT_API FSpawnEnemyData
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSubclassOf<ABaseEnemyCharacter> EnemyClass;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int32 Count = 10;
+};
+
+USTRUCT(BlueprintType)
+struct BULLETANT_API FEnemySpawnerEntry : public FTableRowBase
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int32 WaveIndex = 0;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TArray<FSpawnEnemyData> SpawnEnemyDataArray;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float SpawnInterval = 0.5f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int32 SpawnMinDistance = 1000;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int32 SpawnMaxDistance = 1500;
+};
