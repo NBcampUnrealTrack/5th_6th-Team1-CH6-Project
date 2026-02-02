79f32753782606f586128ce5b9bc7e9d51f1f77c
Derden-95 <khkc9597@gmail.com>
2026-02-02 19:03:39 +0900

    Add Rotate Turret
    - 터렛이 주변 Pawn 감지해서 가장 가까운 타겟에게 회전

--- Source/BulletAnt/Building/BaseTurret.h
+++ Source/BulletAnt/Building/BaseTurret.h
@@ -10,6 +10,7 @@
 
 class UAbilitySystemComponent;
 class URangedWeaponDataAsset;
+class USphereComponent;
 
 UCLASS()
 class BULLETANT_API ABaseTurret : public ABaseBuilding
@@ -26,6 +27,7 @@ protected:
 	virtual void BeginPlay() override;
 
 public:
+	virtual void Tick(float DeltaSeconds) override;
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 
 protected:
@@ -42,6 +44,16 @@ protected:
 	UFUNCTION()
 	void Server_FireTick();
 
+private:
+	UFUNCTION()
+	void OnTargetBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	
+	UFUNCTION()
+	void OnTargetEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UFUNCTION()
+	void UpdateCurrentTarget();
+
 protected:
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<UAbilitySystemComponent> ASC;
@@ -53,4 +65,24 @@ protected:
 	FName MuzzleSocketName = TEXT("Muzzle");
 
 	FTimerHandle FireTimerHandle;
+
+	UPROPERTY(VisibleAnywhere, Category = "Turret|Targeting")
+	TObjectPtr<USphereComponent> TargetSerchingSphere;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Turret|Targeting")
+	float SerchingSphereRadius = 300.f;
+
+	UPROPERTY()
+	TArray<TWeakObjectPtr<AActor>> TargetCandidates;
+
+	UPROPERTY()
+	TWeakObjectPtr<AActor> CurrentTarget;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Turret|Targeting")
+	float TargetSearchInterval = 1.f;
+
+	FTimerHandle TargetSearchTimer;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Turret|Targeting")
+	float TurnSpeedDegPerSec = 180.f;
 };
