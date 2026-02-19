74554c5f1167d0253df48fc0615cb37383c80358
Derden-95 <khkc9597@gmail.com>
2026-02-19 21:20:14 +0900

    [Add] GA_DestroyBuillding

--- Source/BulletAnt/Building/BaseBuilding.cpp
+++ Source/BulletAnt/Building/BaseBuilding.cpp
@@ -7,6 +7,7 @@
 #include "Net/UnrealNetwork.h"
 #include "AbilitySystemComponent.h"
 #include "GAS/AttributeSet/HealthAttributeSet.h"
+#include "GAS/Ability/GA_DestroyBuilding.h"
 #include "GeometryCollection/GeometryCollectionComponent.h"
 #include "GeometryCollection/GeometryCollection.h"
 
@@ -64,6 +65,13 @@ UAbilitySystemComponent* ABaseBuilding::GetAbilitySystemComponent() const
 void ABaseBuilding::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (HasAuthority())
+	{
+		ASC->GiveAbility(FGameplayAbilitySpec(UGA_DestroyBuilding::StaticClass(), 1));
+		HealthSet->SetMaxHealth(5000.f);
+		HealthSet->SetHealth(5000.f);
+	}
 
 	if (DestructionCollection)
 	{
--- Source/BulletAnt/Building/BaseTurret.cpp
+++ Source/BulletAnt/Building/BaseTurret.cpp
@@ -37,7 +37,7 @@ void ABaseTurret::BeginPlay()
 
 	if (HasAuthority())
 	{
-		GiveDefaultAbilities();
+		ASC->GiveAbility(FGameplayAbilitySpec(UGA_Fire::StaticClass(), 1));
 
 		TargetSerchingSphere->OnComponentBeginOverlap.AddDynamic(this, &ABaseTurret::OnTargetBeginOverlap);
 		TargetSerchingSphere->OnComponentEndOverlap.AddDynamic(this, &ABaseTurret::OnTargetEndOverlap);
@@ -157,14 +157,6 @@ void ABaseTurret::OnRep_Dead()
 	if (BarrelMesh)
 	{
 		BarrelMesh->SetHiddenInGame(true);
-	}
-}
-
-void ABaseTurret::GiveDefaultAbilities()
-{
-	if (HasAuthority())
-	{
-		ASC->GiveAbility(FGameplayAbilitySpec(UGA_Fire::StaticClass(), 1));
 	}
 }
 
