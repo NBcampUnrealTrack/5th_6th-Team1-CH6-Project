315fe30ab9a185578fc83224d6e8a3f0650ac8be
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-02-20 21:51:29 +0900

    [Add] Die
    - Die 초기 생성 파일

---
+++ Source/BulletAnt/Enemy/GAS/Ability/GA_Die.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemy/GAS/Ability/GA_Die.h"
+
+UGA_Die::UGA_Die()
+{
+}
+
+void UGA_Die::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+}
---
+++ Source/BulletAnt/Enemy/GAS/Ability/GA_Die.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GA_Die.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BULLETANT_API UGA_Die : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UGA_Die();
+
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData
+	) override;	
+};
