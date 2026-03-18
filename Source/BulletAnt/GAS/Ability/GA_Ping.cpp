e84dac11e5490256339f822c954eb9c1096edde3
ParkMinWoo98 <mypmw13579@gmail.com>
2026-03-18 22:21:27 +0900

    [Update] 핑 위치 불일치 해결 및 서버 동기화 구조 변경

--- Source/BulletAnt/GAS/Ability/GA_Ping.cpp
+++ Source/BulletAnt/GAS/Ability/GA_Ping.cpp
@@ -7,7 +7,7 @@
 UGA_Ping::UGA_Ping()
 {
 	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
-	NetExecutionPolicy = EGameplayAbilityNetExecutionPolicy::ServerOnly;
+	NetExecutionPolicy = EGameplayAbilityNetExecutionPolicy::LocalPredicted;
 
 	FGameplayTagContainer DefaultTag;
 	DefaultTag.AddTag(TAG_Event_Communicate_Ping);
@@ -26,8 +26,22 @@ void UGA_Ping::ActivateAbility(
 		return;
 	}
 
-	PingTrace();
-	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+	UAbilitySystemComponent* ASC = GetAbilitySystemComponentFromActorInfo();
+	if (IsValid(ASC) == false)
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+
+	ASC->AbilityTargetDataSetDelegate(
+		CurrentSpecHandle,
+		CurrentActivationInfo.GetActivationPredictionKey()
+	).AddUObject(this, &UGA_Ping::OnTargetDataReady);
+
+	if (IsLocallyControlled() == true)
+	{
+		SendTargetData();
+	}
 }
 
 void UGA_Ping::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
@@ -35,9 +49,9 @@ void UGA_Ping::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGamepl
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
 }
 
-void UGA_Ping::PingTrace()
+void UGA_Ping::SendTargetData()
 {
-	ABACharacter* Character = Cast<ABACharacter>(CurrentActorInfo->AvatarActor.Get());
+	ABACharacter* Character = Cast<ABACharacter>(GetAvatarActorFromActorInfo());
 	if (IsValid(Character) == false)
 		return;
 
@@ -45,32 +59,69 @@ void UGA_Ping::PingTrace()
 	if (IsValid(PC) == false)
 		return;
 
-	FVector ViewLoc = FVector::ZeroVector;
-	FVector Dir = FVector::ZeroVector;
-	int32 ScreenX = 1920;
-	int32 ScreenY = 1080;
-	PC->GetViewportSize(ScreenX, ScreenY);
-	PC->DeprojectScreenPositionToWorld(
-		(float)ScreenX * 0.5f,
-		(float)ScreenY * 0.5f,
-		ViewLoc,
-		Dir);
+	FVector CamLoc;
+	FRotator CamRot;
+	PC->GetPlayerViewPoint(CamLoc, CamRot);
 
-	FVector Start = ViewLoc;
-	FVector End = ViewLoc + Dir.GetSafeNormal() * TraceDistance;
-	FHitResult HitResult;
-	FCollisionQueryParams CollisionParams;
-	CollisionParams.AddIgnoredActor(Character);
-	bool bHit = GetWorld()->LineTraceSingleByChannel(HitResult, Start, End, ECC_Visibility, CollisionParams);
-	if (bHit)
-	{
-		AActor* HitActor = HitResult.GetActor();
-		if (HitActor)
-		{
-			FGameplayCueParameters Params;
-			Params.Instigator = PC;
-			Params.Location = HitResult.Location;
-			GetAbilitySystemComponentFromActorInfo()->ExecuteGameplayCue(TAG_GameplayCue_Communicate_Ping, Params);
-		}
-	}
+	FVector End = CamLoc + CamRot.Vector() * TraceDistance;
+
+	FHitResult Hit;
+	FCollisionQueryParams Params;
+	Params.AddIgnoredActor(Character);
+
+	GetWorld()->LineTraceSingleByChannel(Hit, CamLoc, End, ECC_Visibility, Params);
+
+	FVector TargetLoc = Hit.bBlockingHit ? Hit.Location : End;
+
+	FGameplayAbilityTargetData_LocationInfo* Data = new FGameplayAbilityTargetData_LocationInfo();
+	Data->TargetLocation.LiteralTransform = FTransform(TargetLoc);
+
+	FGameplayAbilityTargetDataHandle Handle;
+	Handle.Add(Data);
+
+	GetAbilitySystemComponentFromActorInfo()->ServerSetReplicatedTargetData(
+		GetCurrentAbilitySpecHandle(),
+		GetCurrentActivationInfo().GetActivationPredictionKey(),
+		Handle,
+		FGameplayTag(),
+		GetAbilitySystemComponentFromActorInfo()->ScopedPredictionKey);
+}
+
+void UGA_Ping::OnTargetDataReady(const FGameplayAbilityTargetDataHandle& Data, FGameplayTag Tag)
+{
+	if (CurrentActorInfo->IsNetAuthority() == false)
+		return;
+
+	UAbilitySystemComponent* ASC = GetAbilitySystemComponentFromActorInfo();
+
+	ASC->ConsumeClientReplicatedTargetData(
+		CurrentSpecHandle,
+		CurrentActivationInfo.GetActivationPredictionKey());
+
+	if (Data.Num() == 0)
+		return;
+
+	const FGameplayAbilityTargetData_LocationInfo* LocData = static_cast<const FGameplayAbilityTargetData_LocationInfo*>(Data.Get(0));
+	if (!LocData)
+		return;
+
+	FVector PingLocation = LocData->TargetLocation.LiteralTransform.GetLocation();
+
+	ABACharacter* Character = Cast<ABACharacter>(GetAvatarActorFromActorInfo());
+	if (IsValid(Character) == false)
+		return;
+
+	APlayerController* PC = Cast<APlayerController>(Character->GetController());
+	if (IsValid(PC) == false)
+		return;
+
+	FGameplayCueParameters Params;
+	Params.Instigator = PC;
+	Params.Location = PingLocation;
+
+	ASC->ExecuteGameplayCue(
+		TAG_GameplayCue_Communicate_Ping,
+		Params);
+
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
 }
