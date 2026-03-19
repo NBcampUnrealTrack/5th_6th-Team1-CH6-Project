64fb20845fda1a1389ab44cd1b8606fb33f322b9
Derden-95 <khkc9597@gmail.com>
2026-03-19 13:23:11 +0900

    [Add] MissileProjectile

--- Source/BulletAnt/Weapon/Projectile/MissileProjectile.h
+++ Source/BulletAnt/Weapon/Projectile/MissileProjectile.h
@@ -1,17 +1,51 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "Weapon/Projectile/BaseProjectile.h"
 #include "MissileProjectile.generated.h"
 
-/**
- * 
- */
+class UNiagaraSystem;
+class USoundBase;
+class URangedWeaponDataAsset;
+
 UCLASS()
 class BULLETANT_API AMissileProjectile : public ABaseProjectile
 {
 	GENERATED_BODY()
-	
+
+public:
+	AMissileProjectile();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void HandleActorHit(AActor* SelfActor, AActor* OtherActor, FVector NormalImpulse, const FHitResult& Hit);
+	void Explode(const FHitResult& Hit);
+	void ApplyExplosionDamage(const FVector& ExplosionLocation, AActor* DirectHitActor);
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Missile|Explosion", meta = (ClampMin = "0.0"))
+	float ExplosionRadius = 300.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Missile|Explosion", meta = (ClampMin = "0.0"))
+	float ExplosionDamage = 30.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Missile|Explosion")
+	TObjectPtr<UNiagaraSystem> ExplosionEffect = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Missile|Explosion")
+	TObjectPtr<USoundBase> ExplosionSound = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Missile|Explosion")
+	FVector ExplosionEffectScale = FVector(1.f);
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Missile|Explosion", meta = (ClampMin = "0.0"))
+	float ExplosionOffsetAlongNormal = 10.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Missile|Explosion|Debug")
+	bool bDrawExplosionDebug = false;
+
+private:
+	bool bExplosionProcessed = false;
 };
