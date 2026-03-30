74f3cd20b9731c75213d1786fd143ea3f0537347
Derden-95 <khkc9597@gmail.com>
2026-03-30 18:10:39 +0900

    [Update] Set Sound Class

--- Source/BulletAnt/Framework/BAGameInstance.cpp
+++ Source/BulletAnt/Framework/BAGameInstance.cpp
@@ -1,5 +1,26 @@
 ﻿#include "Framework/BAGameInstance.h"
 #include "Framework/MapConfig.h"
+#include "Settings/SettingsSubsystem.h"
+#include "Sound/SoundClass.h"
+#include "Sound/SoundMix.h"
+
+void UBAGameInstance::Init()
+{
+	Super::Init();
+
+	USettingsSubsystem* SettingsSubsystem = GetSubsystem<USettingsSubsystem>();
+	if (!SettingsSubsystem)
+	{
+		return;
+	}
+
+	SettingsSubsystem->ConfigureSoundMix(
+		MasterSoundMix,
+		MasterSoundClass,
+		MusicSoundClass,
+		SfxSoundClass
+	);
+}
 
 void UBAGameInstance::OnStart()
 {
--- Source/BulletAnt/Framework/BAGameInstance.h
+++ Source/BulletAnt/Framework/BAGameInstance.h
@@ -5,6 +5,8 @@
 #include "BAGameInstance.generated.h"
 
 class UMapConfig;
+class USoundMix;
+class USoundClass;
 
 UCLASS()
 class BULLETANT_API UBAGameInstance : public UGameInstance
@@ -12,6 +14,7 @@ class BULLETANT_API UBAGameInstance : public UGameInstance
 	GENERATED_BODY()
 	
 public:
+	virtual void Init() override;
 	virtual void OnStart() override;
 
 	UMapConfig* GetMapConfig() { return MapConfig; }
@@ -19,4 +22,16 @@ public:
 protected:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	TObjectPtr<UMapConfig> MapConfig;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Settings|Audio")
+	TObjectPtr<USoundMix> MasterSoundMix;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Settings|Audio")
+	TObjectPtr<USoundClass> MasterSoundClass;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Settings|Audio")
+	TObjectPtr<USoundClass> MusicSoundClass;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Settings|Audio")
+	TObjectPtr<USoundClass> SfxSoundClass;
 };
